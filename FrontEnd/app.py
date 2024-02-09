3e725f7ab8d2e305075698db1354c85ee319ecb0
ShogyX <victorsjot@gmail.com>
2024-02-09 11:55:01 +0100

    basic webpage with rudimentary functionality

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -6,12 +7,44 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
-def result():
+@app.route('/analyze', methods=['POST'])
+def analyze():
     if request.method == 'POST':
         input_text = request.form['input_text']
-        # Process the input_text as needed
-        return render_template('result.html', input_text=input_text)
+        analysis_result = analyze_text(input_text)
+        #Returns a json doc with information to the client side
+        return jsonify({'result': analysis_result})
+
+def search_special_int_caps(text):
+    results = {
+        "special_characters": 0,
+        "integers": 0,
+        "capitalized_letters": 0
+    }
+    
+    special_characters = re.findall(r'[^a-zA-Z0-9\s]', text)
+    integers = re.findall(r'\d', text)
+    capitalized_letters = re.findall(r'[A-Z]', text)
+    
+    if special_characters:
+        results["special_characters"] = special_characters
+    if integers:
+        results["integers"] = integers
+    if capitalized_letters:
+        results["capitalized_letters"] = capitalized_letters
+    
+    return results
+
+
+
+def analyze_text(text):
+    #not integrated yet
+    special_char = search_special_int_caps(text)
+
+
+
+    return {'Length': len(text), 'First Character': text[0] if text else ''}
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
+
